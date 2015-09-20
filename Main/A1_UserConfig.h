c29e7badc57dc71b398ef39de589397802b536c3
Eldon R. Brown <eldonb46@gmail.com>
2015-09-20 12:12:12 -0700

    ERB_MJ - ReAdded Deleted Files

---
+++ Main/A1_UserConfig.h
@@ -0,0 +1,58 @@
+// A1_UserConfig.h
+
+
+#ifndef USERCONFIG_H
+#define USERCONFIG_H
+
+
+// Note: By my convention, All Global Variables and Constants start with a letter "g".
+
+// Station Config
+    String gSsid = "EBCON";        // Change to your SSID
+    String gPasswd = "........";   // Change to your Passwd
+
+
+// Default Unit ID "Node" Name, This will be changed in Setup
+    char gDeviceName[16] = "Nod000000";
+
+
+// Soft AP Config
+    const String gSoftAPAddress = "192.168.4.1"; // Default Soft AP IP Address
+    const int gDeviceApChannelDefault = 8; // Used when in SoftAP only Mode
+    int gApChannel = gDeviceApChannelDefault;
+
+
+// Initial Automatic Page Refresh Intervals
+    int gAutoHomeRefresh = 180;
+    int gAutoHelpRefresh = 180;
+    int gAutoAdminRefresh = 180;
+
+
+// Time ZONE
+    const long gTimeZONE = -(7*HRs/SECs); // PDT
+
+
+// Used to set LED state
+    const boolean ON = 0;
+    const boolean OFF = !ON;
+
+
+// Pin numbers for LEDs
+    const int gGrnLED = 0;
+    const int gRedLED = 2;
+    const int gBluLED = 4;
+    boolean gRedLedState = OFF;
+
+// My Known Esp Unit ID's
+    const char* knowUnitID[] = {
+      "1083.7620", "Esp13", // Nod129
+      "1053.1281", "Esp11",
+      "1669.8581", "Esp11",
+      "1053.1277", "Esp11", // Nod168
+    };
+    
+#endif // USERCONFIG_H
+
+// ###########################################################
+// End
+
---
+++ Main/A4_WifiCTL.ino
@@ -0,0 +1,145 @@
+// A4_WifiCTL
+
+/*
+ * Copyright (c) 2015, Eldon R. Brown - ERB - WA0UWH - eldonb@ebcon.com
+ *
+ * See my Blog, at: http://WA0UWH.blogspot.com
+ *
+ * See my Source at GitHub, at: https://github.com/wa0uwh/ERB-EspWebServer
+ *
+ * All rights reserved. See FULL Copyright in Main or Info Page for Exclusions
+ *
+ * This Effort was Inspired by work by: Majenko Technologies - 2015 Esp8266 AdvancedWebServer
+ *
+ * See also Arduino IDE, at: https://github.com/esp8266/Arduino
+ */
+
+
+// ###########################################################
+//////////////////////////////////////////////////////////////
+// Initializes the WIFI AP
+//
+void
+//ICACHE_FLASH_ATTR
+initWifiAp()
+{
+
+    gApTimeout = 0;
+    WiFi.mode( WIFI_AP );
+    //WiFi.softAP( gDeviceName, "passwd1234", gApChannel );
+    WiFi.softAP( gDeviceName, 0, gApChannel );  // For Open AP Network
+    yield();
+    Serial.println ( F("\nInitilize: AP Mode") );
+    
+    initHandlers();
+    startWebServer(); 
+    gStnLive = false;
+    gReStartSTN = true;
+    yield();
+}
+
+
+// ###########################################################
+//////////////////////////////////////////////////////////////
+// ReStarts the WIFI AP
+//
+void
+//ICACHE_FLASH_ATTR
+restartWifiAp()
+{
+    
+    //gApKeepLive = true;
+    gApTimeout = millis() + 15 * MINs;
+    WiFi.mode( WIFI_AP_STA );
+    Serial.println ( F("\n\nReStarting: AP Mode") );
+    yield();
+}
+
+
+// ###########################################################
+//////////////////////////////////////////////////////////////
+// Initializes the WIFI STATION
+//
+void
+//ICACHE_FLASH_ATTR
+initWifiStn()
+{
+    
+    WiFi.mode( WIFI_AP_STA );
+    Serial.print ( F("Trying to Connect to: ") );
+ 
+    if (gPasswd.length() == 0) {   
+         Serial.print ( gSsid + F("(open)*") );
+         WiFi.begin ( gSsid.c_str() ); 
+    }
+    else {
+         Serial.print ( gSsid );
+         WiFi.begin ( gSsid.c_str(), gPasswd.c_str() );
+    }
+    yield();
+
+    gWifiStnAttempts = 1;
+}
+
+
+// ###########################################################
+//////////////////////////////////////////////////////////////
+// Trys to Start the WIFI STATION
+//
+void
+//ICACHE_FLASH_ATTR
+try2StartWifiStn()
+{
+    
+    if (!gWifiStnAttempts) initWifiStn(); 
+    
+    delay ( 100 );
+    if ( WiFi.status() != WL_CONNECTED ) {
+    	delay ( 500 );
+    	Serial.print ( "." );
+        gWifiStnAttempts = ++gWifiStnAttempts % 10;
+        return;
+    }
+    yield();
+    
+    gStnLive = true;
+    gReStartSTN = false;
+    gWifiStnAttempts = 0;
+    gWifiReStarts++;
+    
+    // Create Node Name from Least Digits of IPA
+    sprintf(gDeviceName+3, FMT("%0d"), WiFi.localIP() >> 24);              // Append Least IP Digits at position 3
+    
+    
+    Serial.println ( sF("\nConnected to: ")       + String(WiFi.SSID()) );
+    Serial.println ( sF("IP address: ")           + ipa2str(WiFi.localIP()) );
+    Serial.println ( sF("STN Mac Address: ")      + mac2str(WiFi.macAddress(gMacBuf)) );
+    Serial.println ( sF("Soft IP address: ")      + gSoftAPAddress );
+    Serial.println ( sF("Soft AP  Mac Address: ") + mac2str(WiFi.softAPmacAddress(gMacBuf)) );
+    Serial.println ( sF("WiFi ReStarts: ")        + String(gWifiReStarts) );
+    yield(); 
+
+    restartWifiAp();
+    
+    Serial.println (); Serial.println ();
+    WiFi.printDiag(Serial);
+    Serial.println (); Serial.println ();
+  
+}
+
+
+// ###########################################################
+//////////////////////////////////////////////////////////////
+// Starts the WIFI WEB SERVER
+//
+void
+//ICACHE_FLASH_ATTR
+startWebServer()
+{
+    
+    gServer.begin();
+    Serial.println ( F("HTTP Server Started") );
+}
+
+// ###########################################################
+// End
