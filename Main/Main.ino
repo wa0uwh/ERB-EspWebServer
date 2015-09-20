cc17de8da56f3df080e65d993d4153022c72f713
Eldon R. Brown <eldonb46@gmail.com>
2015-09-20 12:02:09 -0700

    ERB_MI - Reorganized Header Files, and Move ALL Send functions to a single file

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,7 +1,20 @@
 //  MainNS
 
+/*
+ * Copyright (c) 2015, Eldon R. Brown - ERB - WA0UWH - eldonb@ebcon.com
+ *
+ * See my Blog, at: http://WA0UWH.blogspot.com
+ *
+ * See my Source at GitHub, at: https://github.com/wa0uwh/ERB-EspWebServer
+ *
+ * All rights reserved. See FULL Copyright in Main or Info Page for Exclusions
+ *
+ * This Effort was Inspired by work by: Majenko Technologies - 2015 Esp8266 AdvancedWebServer
+ *
+ * See also Arduino IDE, at: https://github.com/esp8266/Arduino
+ */
 
-#include "AA_Main.h"
+#include "A1_Main.h"
 
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
@@ -10,131 +23,76 @@
 #include <WiFiClient.h>
 #include <ESP8266mDNS.h>
 
+#include "A1_UserConfig.h"
 
-extern "C" {
-    #include "user_interface.h"
-    uint16 readvdd33(void);
-}
-
-const char *gRev = "ERB_MH";  // Software Revision Code
+const char *gRev = "ERB_MI";  // Software Revision Code
 
 
+    String gSsidPrev = gSsid;
+    String gPasswdPrev = gPasswd;
 
 
-const char COPYRIGHT1[] PROGMEM = R"(
-     /*
-      * Copyright (c) 2015, Eldon R. Brown - ERB - WA0UWH - eldonb@ebcon.com
-      *
-      * See my Blog, at: http://WA0UWH.blogspot.com
-      *
-      * See my Source at GitHub, at: https://github.com/wa0uwh/ERB-EspWebServer
-      *
-      * All rights reserved. See FULL Copyright in Main.ino, or Info Web Page for Exclusions
-      *
-      * This Effort was Inspired by work by: Majenko Technologies - 2015 Esp8266 AdvancedWebServer
-      *
-      * See also Arduino IDE, at: https://github.com/esp8266/Arduino
-      *
-      * Contritutors:
-      *
-      *
-      */
-)";
+    unsigned long gApTimeout = 0;
+    boolean gStnLive = false;
+    int gWifiStnAttempts = 0;
+    int gWifiReStarts = 0;
+    boolean gReStartSTN = false;
 
-
-// Note: By my convention, All Global Variables and Constants start with a letter "g".
-
-
-String gSsid = "EBCON";        // Change to your SSID
-String gPasswd = "........";   // Change to your Passwd
-
-
-String gSsidPrev = gSsid;
-String gPasswdPrev = gPasswd;
-
-char gDeviceName[16] = "Nod000000"; // Default Unit ID "Node" Name, This will be changed in Setup
-
-const String gSoftAPAddress = "192.168.4.1"; // Default Soft AP IP Address
-const int gDeviceApChannelDefault = 8; // Used when in SoftAP only Mode
-int gApChannel = gDeviceApChannelDefault;
-
-unsigned long gApTimeout = 0;
-boolean gStnLive = false;
-int gWifiStnAttempts = 0;
-int gWifiReStarts = 0;
-boolean gReStartSTN = false;
-
-byte gCurrentPage = HOMEPAGE;
+    byte gCurrentPage = HOMEPAGE;
 
 // Buffer, Used with FMT() to store constants in program space (FLASH)
 // See Main.h file\
-char gFmtBuf[64+2];
+    char gFmtBuf[64+2];
 
-char gTmpBuf[32+2];  // Generic Temp Buffer
+    char gTmpBuf[32+2];  // Generic Temp Buffer
 
 // Line Graphic will be plotted with this data
-byte gFreeHeapLog[512]; // Log Storage
-int gFreeHeapLogIndex = 0;
+    byte gFreeHeapLog[512]; // Log Storage
+    int gFreeHeapLogIndex = 0;
 
-const boolean ON = 0;  // Used to set LED state
-const boolean OFF = !ON;
+    boolean gVerbose = true;        // Web Page Verbose Mode
 
-boolean gVerbose = true;        // Web Page Verbose Mode
-
-boolean gVisitorMapState = true; // Show Visitor Map
-String  gSharedVisitorMapIdKey = "2q435yktjwx";   // A shared "IdKey" for sites that want to participate in shared Global Reverse Beacon Map
-String  gVisitorMapIdKey = gSharedVisitorMapIdKey;
-int gVisitorMapStyle = 2;
+    boolean gVisitorMapState = true; // Show Visitor Map
+    String  gSharedVisitorMapIdKey = "2q435yktjwx";   // A shared "IdKey" for sites that want to participate in shared Global Reverse Beacon Map
+    String  gVisitorMapIdKey = gSharedVisitorMapIdKey;
+    int gVisitorMapStyle = 2;
  
-long gHits = 0;                  // Web page Hit Counter
+    long gHits = 0;                  // Web page Hit Counter
 
-// Initial Automatic Page Refresh Intervals
-int gAutoHomeRefresh = 180;
-int gAutoHelpRefresh = 180;
-int gAutoAdminRefresh = 180;
+    unsigned long gNextNTPSchd = millis();
 
+    unsigned long gNextMDnsSchd = millis();
 
-unsigned long gNextNTPSchd = millis();
+    unsigned long gPageStartTime = millis();
 
-unsigned long gNextMDnsSchd = millis();
+    byte gMacBuf[6]; // A buffer to hold Mac Address conversion
 
-byte gMacBuf[6]; // A buffer to hold Mac Address conversion
+    long gIdleL = 0;  // Idle counter
+    long gUpTimeSec = 0; 
 
-unsigned long gPageStartTime = millis();
+    boolean gEpochAvailable = false;
+    unsigned long gEpochDelta = 0; // The Time ADDED to millis() to get Epoch
 
-long gIdleL = 0;  // Idle counter
-long gUpTimeSec = 0; 
+    boolean gMarkFlag = false;
 
-const long gTimeZONE = -(7*HRs/SECs); // PDT
-boolean gEpochAvailable = false;
-unsigned long gEpochDelta = 0; // The Time ADDED to millis() to get Epoch
+    long gSentSize = 0; // WEB Page Size
 
-boolean gMarkFlag = false;
+    long gLoadAvgL = 0;
+    long gLoadAvgCountL = 0;
+    String gLoadAvgS = "1.00";
+    long gIdlePrevL = 0;
+    unsigned long gOneSecSchedule = millis() + 1 * SECs;
 
-long gSentSize = 0; // WEB Page Size
+    MDNSResponder gMDns;
+    boolean gMDnsResponderReady = false;
 
-long gLoadAvgL = 0;
-long gLoadAvgCountL = 0;
-String gLoadAvgS = "1.00";
-long gIdlePrevL = 0;
-unsigned long gOneSecSchedule = millis() + 1 * SECs;
-
-MDNSResponder gMDns;
-boolean gMDnsResponderReady = false;
-
-ESP8266WebServer gServer ( 80 );
-WiFiClient gClient;
+    ESP8266WebServer gServer ( 80 );
+    WiFiClient gClient;
 
 
-// Pin numbers for LEDs
-const int gGrnLED = 0;
-const int gRedLED = 2;
-const int gBluLED = 4;
-boolean gRedLedState = OFF;
-
-// Some often used Text Strings as Constants
-const String gTextPlain = "text/plain";
-const String gTextHtml  = "text/html";
+// Some often used Text Strings
+    #define TEXT_PLAIN "text/plain"
+    #define TEXT_HTML "text/html"
 
 // ###########################################################
 void
@@ -184,7 +142,7 @@ loop ( void )
       
       gServer.handleClient(); 
       
-      // ShortCut the Loop, so Support Tasks only run once per second
+      // ShortCuts the Loop, so Support Tasks only run once per second
       // This keeps the "loop" very fast most of the time, which is needed
       // for WIFI processes.
       if (gOneSecSchedule > millis()) return;
